01f9dcacca67671c654c109f9b270963d225b6c9
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2025-10-23 18:24:20 +0000

    chore(dfs): incomplete

--- src/dfs.rs
+++ src/dfs.rs
@@ -2,9 +2,9 @@ use std::collections::{HashMap, HashSet, VecDeque};
 
 use anyhow::anyhow;
 
-use maze_robot::{Direction, Maze, Robot};
+use maze_robot::{Cell, Direction, Maze, Robot};
 
-type CellGraph = HashMap<usize, [Option<usize>; 4]>;
+type CellGraph = HashMap<usize, Vec<(usize, Direction)>>;
 
 const DIR_ARR: [Direction; 4] = [
     Direction::Up,
@@ -14,14 +14,43 @@ const DIR_ARR: [Direction; 4] = [
 ];
 
 pub fn find_solution<M: Maze>(robot: Robot<M>) -> anyhow::Result<(CellGraph, VecDeque<usize>)> {
-    let mut nxt_idx = 0;
+    let mut nxt_idx: usize = 0;
     let mut location = nxt_idx;
 
-    let mut stack = VecDeque::from([nxt_idx]);
+    let mut stack = VecDeque::from([(nxt_idx, None)]);
     let mut graph: CellGraph = HashMap::new();
     let mut visited = HashSet::new();
 
-    for
+    while let Some(&(idx, dir)) = stack.front() {
+        // mark visited
+        visited.insert(idx);
+        // pop neighbors?
+        let parent = stack.get(1).map(|(parent_idx, parent_dir)| {
+            (
+                parent_idx,
+                parent_dir
+                    .expect("direction will exist if parent exists")
+                    .reverse(),
+            )
+        });
+        let neighbors = DIR_ARR.iter().fold(
+            parent.map_or(HashMap::new(), |(p, d)| HashMap::from([(d, p)])),
+            |acc, dir| {
+                if let None = acc.get(dir) {
+                    match robot.peek(dir) {
+                        Cell::Finish | Cell::Open => {
+                            let cell = nxt_idx;
+                            nxt_idx += 1;
+                            acc.insert(dir, cell)
+                        }
+                        Cell::Wall => acc,
+                    }
+                } else {
+                    acc
+                }
+            },
+        );
+    }
 
     Err(anyhow!("No solution found 😢"))
 }
